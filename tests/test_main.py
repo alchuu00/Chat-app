88a0e8e1b82c13767ac64ebe5828371986660c86
Alja Čekada <alja.cekada@gmail.com>
2022-11-28 21:22:07 +0100

    added automated testing

---
+++ tests/test_main.py
@@ -0,0 +1,75 @@
+import os
+import pytest
+
+os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+
+from main import app, db
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+
+    cleanup()  # clean up before every test
+
+    db.create_all()
+
+    yield client
+
+
+def cleanup():
+    # clean up/delete the DB (drop all tables in the database)
+    db.drop_all()
+
+
+# all test functions must start with test keyword
+def test_index_not_logged_in(client):
+    response = client.get("/")
+    assert b"Enter your name" in response.data
+
+
+def test_index_logged_in(client):
+    # post request test
+    client.post('/login',
+                data={"user-name": "Test User", "user-email": "test@user.com",
+                      "user-password": "password123"},
+                follow_redirects=True)
+
+    response = client.get('/')
+    assert b"Enter your message" in response.data
+
+
+def test_index_add_message(client):
+    test_user = "Test User"
+    client.post("/login",
+                data={"name": test_user, "email": "testuser@test.com", "password": "password123"},
+                follow_redirects=True)
+
+    res = client.get('/',
+                     data={"text": "This is a test message"},
+                     follow_redirects=True)
+
+    test_message = b"This is a test message"
+    response = client.get("/add-message",
+                          data={"text": "This is a test message"},
+                          follow_redirects=True)
+
+    assert test_message in response.data
+
+
+def test_delete_user(client):
+    test_user = "Test User"
+    client.post("/login",
+                data={"name": test_user, "email": "testuser@test.com", "password": "password123"},
+                follow_redirects=True)
+
+    client.post("/profile/delete",
+                data={"name": test_user, "email": "testuser@test.com", "password": "password123"},
+                follow_redirects=True)
+
+    test_message = b"This is a test message"
+    response = client.get("/add-message",
+                          data={"message": "This is a test message"},
+                          follow_redirects=True)
+
+    assert test_message in response.data
